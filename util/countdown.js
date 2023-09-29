790adbcc2c0ee8c76bc2beb15ba78f5cbdf7c29c
danferth <danferth@gmail.com>
2023-09-28 17:04:11 -0700

    countdown cleanup

--- util/countdown.js
+++ util/countdown.js
@@ -4,15 +4,7 @@ function countdown() {
   const currentDateObj = new Date();
   const countdownTimes = createStartTarget(currentDateObj);
   const clockData = findDifference(countdownTimes[0], countdownTimes[1]);
-  // console.log("countdown run", clockData);
   return clockData;
 }
-
-// function intervalCountdown() {
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve(countdown()), 1000);
-//   });
-//   return promise;
-// }
 
 export { countdown };
